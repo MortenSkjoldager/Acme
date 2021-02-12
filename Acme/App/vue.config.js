ab4789247265ff87f37e4b898bc85a44c7495e8f
Morten Skjoldager <msk@ucommerce.net>
2021-02-12 22:24:57 +0100

    vue.config for output assets.

---
+++ Acme/App/vue.config.js
@@ -0,0 +1,24 @@
+﻿module.exports = {
+    outputDir: './../dist',
+    lintOnSave: false,
+    productionSourceMap: false,
+    filenameHashing: false,
+    css: {
+        extract: {
+            filename: '/css/[name].css'
+        }
+    },
+    configureWebpack: {
+        optimization: {
+            splitChunks: false
+        },
+        output: {
+            filename: '/scripts/[name].js'
+        },
+        resolve: {
+            alias: {
+                'vue$': 'vue/dist/vue.esm.js'
+            }
+        }
+    },
+}
