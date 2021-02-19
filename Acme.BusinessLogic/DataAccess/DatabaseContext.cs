3505939e1f4384cf0cf56613e0695d0e985cc096
Morten Skjoldager <msk@ucommerce.net>
2021-02-20 00:57:43 +0100

    Added console application for easy generation of keys.

---
+++ Acme.BusinessLogic/DataAccess/DatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Acme.BusinessLogic.Model;
+
+namespace Acme.BusinessLogic.DataAccess
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Submission> Submissions { get; set; }
+
+        public DbSet<SerialNumber> SerialNumbers { get; set; }
+
+        public DatabaseContext() : base("DatabaseContext")
+        {
+        }
+        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
